73cb8be37f993fba31ed0bd6d9dc40a46e01efa9
Marie <marie.darrigol@e.ujf-grenoble.fr>
2016-06-18 13:04:07 -0400

    BumpMapping class completed a little

--- BumpMapping.cpp
+++ BumpMapping.cpp
@@ -2,10 +2,22 @@
 
 /*
 	Used to compute the normal on the impact point to make the surface "bump"
-	If the impacted object has a bump map, it will get the color value of this point on the map
-	Then, it will compute the normal depending on the color found
-	Finally, it will return that value
+	If the impacted object exists and has a bump map, will get the color value of this point on the map
+	Then, will compute and return the normal depending on the color found
+	Else, will return the standard normal
 */
 Vec3 BumpMapping::computeBump(const pair<Vec3, SceneObject*> impactedObject) const {
+	Vec3 res = Vec3();
+	if (impactedObject.second) {	// Check if the object exist
+
+		// TODO : check if the object has a bump map
+
+		Vec3 objectPosition = impactedObject.second->getPosition();	// Store the position of the object
+		Vec3 positionImpact = impactedObject.first - objectPosition; // Store the position of the impact in the object base
+
+		// TODO : get the matching point on the texture
+
+		// TODO : compute the new normal
+	}
 	return Vec3();
 }
--- BumpMapping.h
+++ BumpMapping.h
@@ -1,8 +1,8 @@
 /*
 	Module used to compute the bump effect on an object
-	Takes the impact and the object return bu the Collider
-	If the object has a bump map, it will return a Vec3 that represents the normal on that point
-	Else, it will return the standard normal on this point
+	Takes the impact and the object return by the Collider
+	If the object exists and has a bump map, will return a Vec3 that represents the normal on that point
+	Else, will return the standard normal on this point
 	This normal will be used by the Phong model
 */
 
