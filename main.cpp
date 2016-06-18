23f8182ed73be0c25ef96b97da545fb0090cf3ac
th0nyXX <anthony.leonard1@uqac.ca>
2016-06-18 18:20:00 -0400

    Rendering loop modified to test collision on SceneObject vector

--- main.cpp
+++ main.cpp
@@ -1,19 +1,18 @@
 #include <Windows.h>
 #include "RenderWindow.h"
 #include "Camera.h"
-#include "Scene.h"
 #include "Sphere.h"
+#include "collision.h"
 #include "Ray.h"
 
-void renderingLoop(RenderWindow& window, Camera& cam, Sphere& sp) {
+void renderingLoop(RenderWindow& window, Camera& cam, vector<SceneObject*> objVect) {
 	
 	for (int x = 0; x < 1024; x++) {
 		for (int y = 0; y < 768; y++) {
 			Ray currRay(cam.getRay(x, y));
+			pair<Vec3, SceneObject*> intersection = collide(currRay, objVect);
 
-			pair<bool, Vec3> intersection = sp.intersect(currRay);
-
-			if (intersection.first)
+			if (intersection.second != nullptr)
 				window.setPixel(x, y, 255, 0, 0);
 			else
 				window.setPixel(x, y, 20, 20, 20);
@@ -24,10 +23,11 @@ void renderingLoop(RenderWindow& window, Camera& cam, Sphere& sp) {
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR lpCmdLine, int nCmdShow) {
 	RenderWindow mainWindow(hInstance, nCmdShow);
 	Camera cam(Vec3(), Vec3(0.0f, 0.0f, 2.0f), 1024, 768);
-	Sphere sp(Vec3(0.5f, 0.0f, 5.0f), 0.5f);
+	vector<SceneObject*> objVect;
+	objVect.push_back(new Sphere(Vec3(0.5, 0.0, 5.0), 0.5));
+	objVect.push_back(new Sphere(Vec3(-1.0, 0.0, 7.0), 1.0));
 
-	renderingLoop(mainWindow, cam, sp);
-
+		renderingLoop(mainWindow, cam, objVect);
 
 	MSG msg = {};
 	while (GetMessage(&msg, NULL, 0, 0)) {
