b7d081588c62535753b3f2fb643bbdb09d2c35df
Sovhan <olivier.soldano@e.ujf-grenoble.fr>
2016-06-20 00:06:43 -0400

    tabification of main

--- main.cpp
+++ main.cpp
@@ -15,12 +15,12 @@ void renderingLoop(RenderWindow& window, Camera& cam, Sphere& sp) {
 			pair<bool, Vec3> intersection = sp.intersect(currRay);
 
 			if (intersection.first) {
-			tmp_color = currRay.phong_shading(intersection.second, Vec3(1.0,1.0,1.0), Material(0.2f, 0.5f, 0.5f, Color(255, 0, 0)),
-				Light());
-			int r = (int)(tmp_color.getX() * 255);
-			int g = (int)(tmp_color.getY() * 255);
-			int b = (int)(tmp_color.getZ() * 255);
-			window.setPixel(x, y, r, g, b);
+				tmp_color = currRay.phong_shading(intersection.second, Vec3(1.0,1.0,1.0), Material(0.2f, 0.5f, 0.5f, Color(255, 0, 0)),
+					Light());
+				int r = (int)(tmp_color.getX() * 255);
+				int g = (int)(tmp_color.getY() * 255);
+				int b = (int)(tmp_color.getZ() * 255);
+				window.setPixel(x, y, r, g, b);
 			} else {
 				window.setPixel(x, y, 20, 20, 20);
 			}
